424360fd1117fe8aa5d298947272a02162c2dd6d
mshynji <m.shynjirobbie@gmail.com>
2026-09-12 15:44:38 -0400

    First commit

---
+++ backend/src/main/java/dev/am/erp/planemp/PlanempApplication.java
@@ -0,0 +1,13 @@
+package dev.am.erp.planemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanempApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/am/erp/planemp/PlanempApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.am.erp.planemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
